4cc0e0c0efbabfdc00afd1cfffc40e022f11c1ed
harshagrawal49 <agrawalharsh0404@gmail.com>
2024-04-08 15:32:20 +0530

    add login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,59 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import {BrowserRouter,Routes,Route, Navigate} from "react-router-dom"
 import Login from "./pages/Login"
-export default function App() {
+import Login from "./pages/Users"
+import Login from "./pages/Trash"
+import Login from "./pages/Taskdetails"
+import Login from "./pages/Dashboard"
+
+
+
+function Layout() {
+  const { user } = useSelector((state) => state.auth);
+
+  const location = useLocation();
+
+  return user ? (
+    <div className='w-full h-screen flex flex-col md:flex-row'>
+      <div className='w-1/5 h-screen bg-white sticky top-0 hidden md:block'>
+        <Sidebar />
+      </div>
+
+      <MobileSidebar />
+
+      <div className='flex-1 overflow-y-auto'>
+        <Navbar />
+
+        <div className='p-4 2xl:px-10'>
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  ) : (
+    <Navigate to='/log-in' state={{ from: location }} replace />
+  );
+}
+
+
+function App() {
   return (
-    <BrowserRouter>
+    <main className='w-full min-h-screen bg-[#f3f4f6] '>
       <Routes>
-        <Route path="/" element={<Login/>}/>
-        <Route path="/Login" element={<Login/>}/>
+        <Route element={<Layout />}>
+          <Route index path='/' element={<Navigate to='/dashboard' />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/tasks' element={<Tasks />} />
+          <Route path='/completed/:status' element={<Tasks />} />
+          <Route path='/in-progress/:status' element={<Tasks />} />
+          <Route path='/todo/:status' element={<Tasks />} />
+          <Route path='/team' element={<Users/>} />
+          <Route path='/trashed' element={<Trash />} />
+          <Route path='/task/:id' element={<TaskDetails />} />
+        </Route>
+
+        <Route path='/log-in' element={<Login />} />
       </Routes>
-    </BrowserRouter>
-  )
+
+      <Toaster richColors />
+    </main>
+  );
 }
